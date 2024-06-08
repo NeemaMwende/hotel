8411b9a20fbb56785f76f60fcf5e8fb9a6fbdec7
NeemaMwende <gracepete12@gmail.com>
2024-06-08 16:12:33 +0300

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,51 +3,6 @@ import logo from "../Images/logo.svg"
 
 const Navbar = () => {
   return (
-   <>
-    <div className="topbar">
-    <div className="container">
-
-      <address className="topbar-item">
-        <div className="icon">
-          <ion-icon name="location-outline" aria-hidden="true"></ion-icon>
-        </div>
-
-        <span className="span">
-          Restaurant St, Delicious City, Nairobi 9578, KE
-        </span>
-      </address>
-
-      <div className="separator"></div>
-
-      <div className="topbar-item item-2">
-        <div className="icon">
-          <ion-icon name="time-outline" aria-hidden="true"></ion-icon>
-        </div>
-
-        <span className="span">Daily : 8.00 am to 10.00 pm</span>
-      </div>
-
-      <a href="tel:+25476812345" className="topbar-item link">
-        <div className="icon">
-          <ion-icon name="call-outline" aria-hidden="true"></ion-icon>
-        </div>
-
-        <span className="span">+254 768 456 7890</span>
-      </a>
-
-      <div className="separator"></div>
-
-      <a href="mailto:booking@restaurant.com" className="topbar-item link">
-        <div className="icon">
-          <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-        </div>
-
-        <span className="span">booking@restaurant.com</span>
-      </a>
-
-    </div>
-  </div>
-
   <header className="header" data-header>
     <div className="container">
 
@@ -148,7 +103,6 @@ const Navbar = () => {
 
     </div>
   </header>
-   </>
   )
 }
 
